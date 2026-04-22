49454c5d87ebedff5597c22d196aba98827b7bd8
SYK-351 <seethirajukrishna@gmail.com>
2026-04-23 00:14:24 +0530

    fix: resolve merge conflict in main.jsx, merge community routes with main

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import WatchPage from './pages/WatchPage/WatchPage'
 import CommunityPage from './pages/CommunityPage/CommunityPage'
 import CommunityChat from './pages/CommunityChat/CommunityChat'
+import Home from './pages/Home'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<WatchPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/watch" element={<WatchPage />} />
         <Route path='/community' element={<CommunityPage />} />
         <Route path='/community/:domain' element={<CommunityChat />} />
       </Routes>
